49e46d3ca795ba126f6b6fbf66da1b896e70f4ba
Chris Pineda <pineda.christopher@gmail.com>
2016-12-22 21:52:50 -0500

    Create class Map

--- W1D4/adts.rb
+++ W1D4/adts.rb
@@ -39,3 +39,35 @@ class Queue # FIFO
     queue
   end
 end
+
+class Map
+  attr_reader :map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    map.each do |pair|
+      if pair.include?(key)
+        pair[1] = value
+        return pair
+      end
+    end
+    map << [key, value]
+  end
+
+  def lookup(key)
+    map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
+  end
+
+  def remove(key)
+    map.delete_if { |pair| pair[0] == key }
+  end
+
+  def show
+    map
+  end
+end
